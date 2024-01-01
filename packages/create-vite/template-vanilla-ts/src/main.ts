371610be5e3269a592d57235244dd427535a5e63
pirtor <pirtorle@gmail.com>
2024-01-01 20:14:40 +0800

    feat(create-vite): setup tailwind for template-vanilla-ts

--- packages/create-vite/template-vanilla-ts/src/main.ts
+++ packages/create-vite/template-vanilla-ts/src/main.ts
@@ -1,1 +1,5 @@
-console.log("hello world!");
+import "./index.css";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+app.innerHTML = `
+  <h1 class="tw-text-4xl tw-font-extrabold">Hello World</h1>
+`;
